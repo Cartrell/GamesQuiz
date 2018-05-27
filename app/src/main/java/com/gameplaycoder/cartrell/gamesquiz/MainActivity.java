512b2116d25cf0e51e2820c32e8e0e871ddb97e2
Cartrell <zir0@sbcglobal.net>
2018-05-27 11:27:15 -0400

    Removed unused code

--- app/src/main/java/com/gameplaycoder/cartrell/gamesquiz/MainActivity.java
+++ app/src/main/java/com/gameplaycoder/cartrell/gamesquiz/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.gameplaycoder.cartrell.gamesquiz.databinding.ActivityMainBinding;
